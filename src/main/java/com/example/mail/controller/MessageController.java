2d4ff4889a433278829cb25452993f88a500ed55
Marko Krizan <markokrizan64@gmail.com>
2020-07-07 01:21:28 +0200

    Switch message to different folder

--- src/main/java/com/example/mail/controller/MessageController.java
+++ src/main/java/com/example/mail/controller/MessageController.java
@@ -4,12 +4,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.mail.security.CurrentUser;
 import com.example.mail.security.UserPrincipal;
 import com.example.mail.service.MessageIndexService;
 import com.example.mail.exception.BadRequestException;
-// import com.example.mail.model.Folder;
+import com.example.mail.model.Folder;
 import com.example.mail.model.Message;
 import com.example.mail.payload.MessageRequest;
 import com.example.mail.repository.FolderRepository;
@@ -23,14 +24,16 @@ public class MessageController {
     private MessageRepository messageRepository;
 
     @Autowired
+    private MessageIndexService messageIndexService;
+
+    @Autowired
     private ModelMapper modelMapper;
 
     @Autowired
-    private MessageIndexService messageIndexService;
-
-    // @Autowired
-    // private FolderRepository folderRepository;
+    private FolderRepository folderRepository;
     
+    //TODO: Refactor required
+    @Transactional
     @PostMapping("/messages")
     @PreAuthorize("hasRole('USER')")
     public Message save(@RequestBody MessageRequest messageRequest, @CurrentUser UserPrincipal currentUser) 
@@ -41,17 +44,28 @@ public class MessageController {
             throw new BadRequestException("Message not found!");
         }
 
-        // Folder folder = folderRepository.findById(messageRequest.getFolderId()).orElse(null);
+        
+        Folder folder = folderRepository.findById(messageRequest.getFolderId()).orElse(null);
 
-        // if(folder == null) {
-        //     throw new BadRequestException("Folder not found!");
-        // }
+        if(folder == null) {
+            throw new BadRequestException("Folder not found!");
+        }
+
+        /**
+         *  Dirty fix - directly change the foreign key due to mapping issues
+         */
+        Folder originalFolder = message.getFolder();
+        messageRequest.setFolderId(originalFolder.getId());
 
         modelMapper.map(messageRequest, message);
+        Message savedMessage = messageRepository.save(message);         
+        messageRepository.updateFolderRelation(folder.getId(), savedMessage.getId());
 
-        Message savedMessage = messageRepository.save(message);             
+        //Set the new folder again to properly reindex the change in es:
+        savedMessage.setFolder(folder);
+        
         messageIndexService.upsert(savedMessage);
 
-        return message;
+        return savedMessage;
     }
 }
